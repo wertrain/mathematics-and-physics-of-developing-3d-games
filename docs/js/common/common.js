32867e00179f6ce011f221126cd92ff24d47b1f7
wertrain <wertrain@gmail.com>
2019-12-08 14:37:37 +0900

    Revert "不要フォルダの削除とタイトルの変更"
    This reverts commit c4f743c3ee5a118d70376ddb6203352dc87084c2.

---
+++ docs/js/common/common.js
@@ -0,0 +1,46 @@
+var MathematicsAndPhysicsOfDeveloping3DGames = {};
+{
+    'use strict';
+
+    MathematicsAndPhysicsOfDeveloping3DGames.Common = {};
+
+    /**
+     * 先頭の半角スペースとタブを（半ば決め打ちで）カット
+     *
+     */
+    let CreateEditorText = function(shaderElementId) {
+        let shaderElement = document.getElementById(shaderElementId);
+        let allCode = shaderElement.innerHTML;
+        let allCodeArray = allCode.split(/\r\n|\r|\n/);
+        // 現在のシェーダー記述方法に合わせて
+        // 先頭の半角スペースとタブを（半ば決め打ちで）カットする
+        let editorText = '';
+        for (let i = allCodeArray[0].length === 0 ? 1 : 0; i < allCodeArray.length - 1; ++i) {
+            let temp = allCodeArray[i];
+            let spaceSize = 4;
+            if (temp[0] === ' ')
+                temp = temp.substring(spaceSize * 2, temp.lenght)
+            else if (temp[0] === '	')
+                temp = temp.substring(1 * 2, temp.lenght)
+            editorText += temp + '\r\n';
+        }
+        return editorText;
+    };
+
+    /**
+     * 共通部分を初期化する。
+     *
+     */
+    MathematicsAndPhysicsOfDeveloping3DGames.Common.initialize = function() {
+        let aceUtil = new MathematicsAndPhysicsOfDeveloping3DGames.AceUtil();
+        let text = CreateEditorText('js');
+        aceUtil.presetEditorText('editor', text);
+        aceUtil.create('editor');
+    };
+
+    MathematicsAndPhysicsOfDeveloping3DGames.Common.run = function(drawFunc) {
+        let canvas = document.getElementById("canvas");
+        let context = canvas.getContext("2d");
+        drawFunc(context);
+    };
+}
